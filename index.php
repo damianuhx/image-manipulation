d0bf488ad306f5ecf005f92355f0f31b576947cb
damianuhx <info@uhx.ch>
2024-06-03 14:54:51 +0200

    Added warning message

--- index.php
+++ index.php
@@ -15,5 +15,8 @@ Available operations after uploading a PNG Image:<ul>
     <li>Transform a normalmap to an lighting-overlay. This can be used to make animations in Spine Animation look more plastic.<br/>
     <img src="normalinput.png"> <img src="normaloutput.png"> <img src="normaloutputinverted.png">
     </li>
+
+    Please note that if multiple users use this app at the same time uploaded images can be overwritten by others. 
+    In very rare cases you will see a image from someone else manipulated instead of your own. In this case: Just try again.
 </body>
 </html>
